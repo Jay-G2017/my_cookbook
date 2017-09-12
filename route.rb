a3e0314f2809d269c77dbec66f3cd72e4ad943b2
Jay <jay.gao@139.com>
2017-09-12 21:16:28 +0800

    change the view of input

--- route.rb
+++ route.rb
@@ -1,10 +1,14 @@
 def run(cookbook_controller)
+  print `clear`
   puts "** Welcome to Jay's CookBook ! **"
   puts "** The world's best easy to use cookbook! **"
+  puts "          ------------------       "
   i = 0
-  while i != 3 do 
+  while i != 3 do
+    print `clear` if i != 0
     puts "Here's the recipe you have:"
     cookbook_controller.index
+    puts ""
     puts "press [1] to create a new recipe"
     puts "press [2] to delete a recipe"
     puts "press [3] to exit!"
@@ -13,8 +17,12 @@ def run(cookbook_controller)
 
     case i
     when 1
+      print `clear`
       cookbook_controller.create
     when 2
+      print `clear`
+      cookbook_controller.index
+      puts ""
       cookbook_controller.delete
     when 3
 
